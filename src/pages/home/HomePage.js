70f026b9cb561ff6e948d95f18a955aab2a5fb73
justinbather <justinbather@hotmail.com>
2023-10-31 18:59:42 -0400

    adds homepage verification

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,8 +1,41 @@
 import { Navbar } from "../../components/Navbar";
 import "../../assets/styles/partials/_components.scss";
 import "../../assets/styles/pages/home.scss";
+import { useEffect } from "react";
+import axios from "axios";
+import { BASE_URL } from "../../config/constants";
+import { useNavigate } from "react-router-dom";
 
 export const HomePage = () => {
+  const navigate = useNavigate();
+
+  const verifyUser = async () => {
+    try {
+      const response = await axios.get(
+        `${BASE_URL}/auth/verify`,
+
+        {
+          headers: {
+            "Content-Type": "application/json",
+            withCredentials: true,
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        return;
+      } else {
+        navigate("/login");
+      }
+    } catch (err) {
+      console.error(err);
+      navigate("/login");
+    }
+  };
+
+  useEffect(() => {
+    verifyUser();
+  }, []);
   return (
     <>
       <Navbar />
