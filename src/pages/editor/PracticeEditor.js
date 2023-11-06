c889d6bb509a95b3e6a4afab7c84045969872331
justinbather <justinbather@hotmail.com>
2023-11-06 11:57:26 -0500

    prep for queue refactoring

--- src/pages/editor/PracticeEditor.js
+++ src/pages/editor/PracticeEditor.js
@@ -2,18 +2,15 @@ import MonacoEditor from "react-monaco-editor";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
-import { BASE_URL, COMPILE_URL, MANAGER_URL } from "../../config/constants";
+import { BASE_URL, MANAGER_URL } from "../../config/constants";
 import "../../assets/styles/pages/editor.scss";
-
-import { Navbar } from "../../components/Navbar";
 
 export const PracticeEditor = () => {
   const [problem, setProblem] = useState(null);
   const [userCode, setUserCode] = useState("");
   const [output, setOutput] = useState("");
   const [errors, setErrors] = useState("");
-  const [hints, setHints] = useState("");
-  const [result, setResult] = useState("");
+  const [testing, setTesting] = useState(false);
 
   const { problemTitle } = useParams();
 
@@ -29,9 +26,10 @@ export const PracticeEditor = () => {
         }
       );
 
-      console.log(response.data.problem);
       setProblem(response.data.problem);
       setUserCode(response.data.problem.userStarterCode);
+
+      setOutput(response.data.output);
     } catch (err) {
       console.error(err);
     }
@@ -40,20 +38,33 @@ export const PracticeEditor = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
+      setErrors("");
+      setOutput("");
+      setTesting(true);
       const response = await axios.post(
-        `${MANAGER_URL}/test`,
+        `${MANAGER_URL}/compile`,
         {
           code: userCode,
           problem: problem,
         },
         {
           headers: { "Content-Type": "application/json" },
-          withCredentials: false,
+          withCredentials: true,
         }
       );
-
-      console.log(response.data);
+      setTesting(false);
+      switch (response.data.success) {
+        case true: {
+          setOutput(response.data.output);
+          break;
+        }
+        case false: {
+          setErrors(response.data.output);
+          break;
+        }
+      }
     } catch (err) {
+      console.log(err);
       console.error("error occured sending request to compile -> ", err);
     }
   };
@@ -70,27 +81,34 @@ export const PracticeEditor = () => {
 
   return (
     <>
-      <div className="container">
-        <div className="container__left">
-          <h3 className="container__title">Two Sum</h3>
-          <div className="container__description">
+      <div className="editor-container">
+        <div className="editor-container__left">
+          <h3 className="editor-container__title">Two Sum</h3>
+          <div className="editor-container__description">
             <p>
               Given the root of a binary tree, return the inorder traversal of
               its nodes' values.
             </p>
           </div>
         </div>
-        <div className="container__right">
-          <MonacoEditor
-            className="editor"
-            width="800"
-            height="600"
-            language="javascript"
-            theme="vs-dark"
-            value={userCode}
-            onChange={handleChange}
-          />
-          <div className="container__button-cont">
+        <div className="editor-container__right">
+          <div className="editor-container__editor-wrapper">
+            <MonacoEditor
+              className="editor"
+              width="750"
+              height="600"
+              language="javascript"
+              theme="vs-dark"
+              value={userCode}
+              onChange={handleChange}
+              options={{
+                minimap: {
+                  enabled: false,
+                },
+              }}
+            />
+          </div>
+          <div className="editor-container__button-cont">
             <Link to="/home">
               <button className="button button--danger">Quit</button>
             </Link>
@@ -102,7 +120,23 @@ export const PracticeEditor = () => {
             </button>
           </div>
           <label className="output-cont__label">Console</label>
-          <div className="output-cont"></div>
+          <div className="output-cont">
+            {output ? (
+              <p>
+                <span className="output-cont__success">~ success: </span>
+                {output}
+              </p>
+            ) : errors ? (
+              <p>
+                <span className="output-cont__failure">~ failed: </span>
+                {errors}
+              </p>
+            ) : testing ? (
+              <p className="output-cont__testing">~ testing...</p>
+            ) : (
+              <p>~</p>
+            )}
+          </div>
         </div>
       </div>
     </>
